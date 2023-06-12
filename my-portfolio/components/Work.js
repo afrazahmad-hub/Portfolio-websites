1dc65938b1c09595b6b1fa766c4cfa02db42ece6
afrazahmad <afrazahmad313@gmail.com>
2023-06-12 13:19:43 +0500

    Added nw CV in my portfolio

--- my-portfolio/components/Work.js
+++ my-portfolio/components/Work.js
@@ -100,17 +100,6 @@ const Work = () => {
           </div>
         </div>
       </div>
-      {/* <div className=" grid grid-cols-3 gap-4">
-        <div className=" relative group">
-          <img src={"/me.jpeg"} alt="" />
-          <div
-            className="bg-[#000000bd] absolute w-[100%] h-[100%] top-0 opacity-0 transition 
-            duration-500 group-hover:opacity-100 grid items-center text-white "
-          >
-            project title
-          </div>
-        </div>
-      </div> */}
     </section>
   );
 };
